041c72b1be2fb4f9dabec50dcf36917bc4717152
harshit srivastava <srivastavaharshit333@gmail.com>
2021-05-06 17:44:42 +0530

    title changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,9 @@ export default () => {
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>{title || 'Gatsby Simplefolio'}</title>
+        <title>{title || 'Harshit Srivastava portfolio'}</title>
         <html lang={lang || 'en'} />
-        <meta name="description" content={description || 'Gatsby Simplefolio'} />
+        <meta name="description" content={description || 'Harshit Srivastava portfolio'} />
       </Helmet>
       <App />
     </>
